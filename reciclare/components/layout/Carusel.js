7b58d7a272961d0ef1ede83252b1558e7009b429
Diana <diana2000d@yahoo.com>
2022-09-18 16:43:14 +0300

    Modificat carusel

--- reciclare/components/layout/Carusel.js
+++ reciclare/components/layout/Carusel.js
@@ -2,23 +2,29 @@ import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Image from 'next/image';
 import logo from '/images/Logo.png';
-
-
+import carusel1 from '/images/carusel1.png';
+import carusel2 from '/images/carusel2.png';
+import carusel3 from '/images/carusel3.png';
+import carusel4 from '/images/carusel4.png';
 
 export default function CarouselComponent() {
     return (
         <div className="carousel-wrapper" >
             <Carousel >
                 <div>
-                    <Image alt="Logo" src={logo} width={800}
-                        height={400} />
+                    <Image alt="carusel1" src={carusel1} width={1846}
+                        height={500} />
                 </div>
                 <div>
-                    <Image alt="Logo" src={logo} width={800}
-                        height={400} />
+                    <Image alt="carusel2" src={carusel2} width={1846}
+                        height={500} />
                 </div>
                 <div>
-                    <Image alt="Logo" src={logo} />
+                    <Image alt="carusel3" src={carusel3} width={1846}
+                        height={500} />
+                </div>
+                <div>
+                    <Image alt="carusel4" src={carusel4} />
                 </div>
             </Carousel>
         </div>
