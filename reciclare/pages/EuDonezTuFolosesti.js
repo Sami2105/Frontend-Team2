fc9bf21f5acc370c7978e5fe7bd9843316e26883
Ema <emanuela.lupancu@student.usv.ro>
2022-09-19 14:01:39 +0300

    am adaugat anunt pentru donare

--- reciclare/pages/EuDonezTuFolosesti.js
+++ reciclare/pages/EuDonezTuFolosesti.js
@@ -4,6 +4,8 @@ import Cards from "components/layout/Card";
 import { Container, Grid, Typography, Avatar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
+
+
 const useStyles = makeStyles((theme) => ({
   snsIcon: {
     width: "30px",
@@ -27,51 +29,28 @@ const EuDonezTuFolosesti = () => {
   return (
     <Layout
       // type your page title and page description.
-      title=" Puncte de reciclare "
-      description="Gaseste cele mai aproapiate puncte de colectare"
+      title=" ANUNȚURI "
+      description="..."
     >
-      <Container maxWidth="md">
-        <Grid container direction="column" spacing={8}>
+      <Container maxWidth="lg">
+        <Grid container direction="column" spacing={8} alignItems="right">
           <Grid item>
-            <Typography variant="h1" align="center" gutterBottom>
-            Eu Donez Tu Folosesti! 
+            <Typography variant="h1" align="left" gutterBottom>
+            ANUNȚURI
             </Typography>
-            <Typography variant="h2" align="center">
-            Gaseste cele mai aproapiate puncte de colectare
+            <Typography variant="h2" align="left">
+            ...
             </Typography>
           </Grid>
-          <Grid item container spacing={2} alignItems="center">
+          <Grid item container spacing={4} alignItems="right">
             <Grid
               item
               container
-              md={4}
+              md={2}
               direction="column"
-              alignItems="center"
-              spacing={2}
+              alignItems="right"
+              spacing={4}
             >
-             
-              <Grid item>
-                <Typography variant="h3">John Doe</Typography>
-              </Grid>
-             
-            </Grid>
-            <Grid item container md={8}>
-              <Typography variant="body1">
-                <br />
-                Contrary to popular belief, Lorem Ipsum is not simply random
-                text. It has roots in a piece of classical Latin literature from
-                45 BC, making it over 2000 years old. Richard McClintock, a
-                Latin professor at Hampden-Sydney College in Virginia, looked up
-                one of the more obscure Latin words, consectetur, from a Lorem
-                Ipsum passage, and going through the cites of the word in
-                classical literature, discovered the undoubtable source. Lorem
-                Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus
-                Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero,
-                written in 45 BC. This book is a treatise on the theory of
-                ethics, very popular during the Renaissance. The first line of
-                Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line
-                in section 1.10.32.
-              </Typography>
             </Grid>
           </Grid>
         </Grid>
@@ -80,5 +59,7 @@ const EuDonezTuFolosesti = () => {
     </Layout>
   );
 };
+
+
 
 export default EuDonezTuFolosesti;
