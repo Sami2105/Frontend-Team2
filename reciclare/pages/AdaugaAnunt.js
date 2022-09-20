fbfaa0d7e1b955c6634aa39e1de6b74ba4356a0f
Nechifor I. Maria Bianca <bianca.nechifor@student.usv.ro>
2022-09-20 23:43:25 +0300

    Create upload image

--- reciclare/pages/AdaugaAnunt.js
+++ reciclare/pages/AdaugaAnunt.js
@@ -1,142 +1,179 @@
-import React from 'react';
 import Link from "components/Link";
 import Layout from "components/layout/Layout";
-import Image from 'next/image';
-import logo from '/images/Logo.png';
+import Image from "next/image";
+import logo from "/images/Logo.png";
 import { Container, Grid, Typography, Avatar } from "@material-ui/core";
-import { FacebookLoginButton, GoogleLoginButton, InstagramLoginButton } from "react-social-login-buttons";
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import {
+  FacebookLoginButton,
+  GoogleLoginButton,
+  InstagramLoginButton,
+} from "react-social-login-buttons";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
+import React, { useReducer } from "react";
+import DropZone from "components/layout/DropZone";
+import styles from "styles/Home.module.css";
+const LogIn = () => {
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "SET_IN_DROP_ZONE":
+        return { ...state, inDropZone: action.inDropZone };
+      case "ADD_FILE_TO_LIST":
+        return { ...state, fileList: state.fileList.concat(action.files) };
+      default:
+        return state;
+    }
+  };
 
-const LogIn= () => {
-    
-    
-    return (
-        <Layout
+  const [data, dispatch] = useReducer(reducer, {
+    inDropZone: false,
+    fileList: [],
+  });
+
+  // destructuring state and dispatch, initializing fileList to empty array
+
+  return (
+    <Layout
       // type your page title and page description.
       title="Adaugă anunț"
       description="Ai produse de donat sau cauți donator? Postează aici."
     >
-
-
-         <Grid container spacing={8} justify="center" style={{ margin: "1em 1.5em" }}>
-
-        <Grid item spacing={12} justify="center" style={{ margin: "0 2em" }}>
-        
-    
-        <Box
-       
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '70ch',display: 'flex', flexWrap: 'wrap' },
-      }}
-      noValidate
-      autoComplete="off"
-      
-       
-    >
-          <Typography variant="h1" align="center" gutterBottom>
-           Adaugă anunț
+      <Grid
+        container
+        spacing={8}
+        justify="center"
+        style={{ margin: "1em 1.5em" }}
+      >
+        <Grid item spacing={10} justify="center" style={{ margin: "0 2em" }}>
+          <Box
+            component="form"
+            sx={{
+              "& .MuiTextField-root": {
+                m: 1,
+                width: "70ch",
+                display: "flex",
+                flexWrap: "wrap",
+              },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <Typography variant="h1" align="center" gutterBottom>
+              Adaugă anunț
             </Typography>
 
-        <Grid container spacing={0} justify="center" style={{ margin: "1.2em 0" }}>
-        <div>
-      <TextField
-          required
-          id="outlined-required"
-          label="Obiectul donat"
-          defaultValue="Obiectul donat"
-          sx={{ m: 1, width: '100ch' }}
-        />       
-      </div>
+            <Grid
+              container
+              spacing={0}
+              justify="center"
+              style={{ margin: "1.2em 0" }}
+            >
+              <div>
+                <TextField
+                  required
+                  id="outlined-required"
+                  label="Obiectul donat"
+                  defaultValue="Obiectul donat"
+                  sx={{ m: 1, width: "100ch" }}
+                />
+              </div>
 
-      <div >
-      <TextField
-          required
-          id="outlined-required"
-          label="Tipul obiectului"
-          defaultValue="Tipul obiectului"
-          sx={{ m: 1, width: '100ch' }}
-        />       
-      </div>
+              <div>
+                <TextField
+                  required
+                  id="outlined-required"
+                  label="Tipul obiectului"
+                  defaultValue="Tipul obiectului"
+                  sx={{ m: 1, width: "100ch" }}
+                />
+              </div>
 
-      <div>
-      <TextField
-          required
-          id="outlined-required"
-          label="Descriere"
-          defaultValue="Descriere"
-          sx={{ m: 1, width: '100ch' }}
-        />   
-      </div>
+              <div>
+                <TextField
+                  required
+                  id="outlined-required"
+                  label="Descriere"
+                  defaultValue="Descriere"
+                  sx={{ m: 1, width: "100ch" }}
+                />
+              </div>
 
-      <div>
-      <TextField
-          required
-          id="outlined-required"
-          label="Starea obiectului"
-          defaultValue="Starea obiectului"
-          sx={{ m: 1, width: '100ch' }}
-        />   
-      </div>
+              <div>
+                <TextField
+                  required
+                  id="outlined-required"
+                  label="Starea obiectului"
+                  defaultValue="Starea obiectului"
+                  sx={{ m: 1, width: "100ch" }}
+                />
+              </div>
 
-      <div>
-      <TextField
-          required
-          id="outlined-required"
-          label="Dimensiuni obiect"
-          defaultValue="Dimensiuni obiect"
-          sx={{ m: 1, width: '100ch' }}
-        />
-      </div>
+              <div>
+                <TextField
+                  required
+                  id="outlined-required"
+                  label="Dimensiuni obiect"
+                  defaultValue="Dimensiuni obiect"
+                  sx={{ m: 1, width: "100ch" }}
+                />
+              </div>
 
-      <div>
-      <TextField
-          required
-          id="outlined-required"
-          label="Greutate"
-          defaultValue="Greutate"
-          sx={{ m: 1, width: '100ch' }}
-        />
-      </div>
+              <div>
+                <TextField
+                  required
+                  id="outlined-required"
+                  label="Greutate"
+                  defaultValue="Greutate"
+                  sx={{ m: 1, width: "100ch" }}
+                />
+              </div>
 
-      <div>
-      <TextField
-          required
-          id="outlined-required"
-          label="Număr de telefon"
-          defaultValue="Număr de telefon"
-          sx={{ m: 1, width: '100ch' }}
-        />
-      </div>
+              <div>
+                <TextField
+                  required
+                  id="outlined-required"
+                  label="Număr de telefon"
+                  defaultValue="Număr de telefon"
+                  sx={{ m: 1, width: "100ch" }}
+                />
+              </div>
 
-      <div>
-      <TextField
-          required
-          id="outlined-required"
-          label="Adresă"
-          defaultValue="Adresă"
-          sx={{ m: 0.4, width: '100ch' }}
-        />
-      </div>
-      
-      <Grid container spacing={0} justify="center" style={{ margin: "1.2em 0" }}>
-      <Button 
-      style = {{width: ' 140ch',height: '8ch' }}
-       variant="outlined"  >Adaugă</Button>
-      </Grid>
+              <div>
+                <TextField
+                  required
+                  id="outlined-required"
+                  label="Adresă"
+                  defaultValue="Adresă"
+                  sx={{ m: 0.4, width: "100ch" }}
+                />
+              </div>
 
-     </Grid>
+              <Grid container style={{ margin: "0 11rem " }}>
+                <DropZone data={data} dispatch={dispatch} />
+              </Grid>
 
-     
-      
-    </Box>
-          </Grid>
+              <Grid
+                container
+                spacing={0}
+                justify="center"
+                style={{ margin: "1.2em 0" }}
+              >
+                <Button
+                  style={{ width: " 140ch", height: "8ch" }}
+                  variant="outlined"
+                >
+                  Adaugă
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
         </Grid>
-        
-        </Layout>
-    )
-}
+      </Grid>
+    </Layout>
+  );
+};
 
-export default LogIn
+export default LogIn;
--- reciclare/pages/index.js
+++ reciclare/pages/index.js
@@ -1,11 +1,18 @@
 import Layout from "components/layout/Layout";
-import { Container, Grid, Typography, Avatar, Button, Item } from "@material-ui/core";
+import {
+  Container,
+  Grid,
+  Typography,
+  Avatar,
+  Button,
+  Item,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import CarouselComponent from "components/layout/Carusel";
 import ImgMediaCard from "components/layout/CarduriArticole";
-import logo from '/images/Logo.png';
+import logo from "/images/Logo.png";
 import Link from "components/Link";
-import Image from 'next/image';
+import Image from "next/image";
 
 const useStyles = makeStyles((theme) => ({
   btn: {
@@ -33,60 +40,97 @@ const About = () => {
     >
       <CarouselComponent></CarouselComponent>
 
-      <Grid container spacing={8} justify="center" style={{ margin: "1.2em 0" }}>
-           <Grid item xs={4}>
-           <h4 className="text-xl mb-1 font-semibold">
-           Ne dorim sa facem planeta un loc mai verde pentru o viata mai sanatoasa. 
-           Avem nevoie de oameni dedicati vin-o in echipa noastra. 
-           Putem sesiza probleme, invata impreuna despre recilat si pe langa acestea putem schimba obiecte intre noi.
-                      </h4>
-                      <Button variant="contained" href="/Inregistrare">Sign Up</Button>
-           </Grid>
-           <Grid item xs={4} >
-           <h4 className="text-xl mb-1 font-semibold">
-           Tu reciclezi, firmele te premiaza. Afla despre campaniile de reciclare.                       </h4>
-                      <Button variant="contained" href="/CampaniiReciclare">Campanii active</Button>
-          </Grid>
-          
-        </Grid>
-      
-        <Grid container spacing={8} justify="center" style={{ margin: "1.2em 0" }}>
+      <Grid
+        container
+        spacing={8}
+        justify="center"
+        style={{ margin: "1.2em 0" }}
+      >
         <Grid item xs={4}>
           <h4 className="text-xl mb-1 font-semibold">
-          Afla unde sunt cele mai apropiate puncte de colectare.  
-                      </h4>
-                      <Button variant="contained" href="/PuncteReciclare">Puncte de colectare</Button>
-          </Grid>
-          <Grid item xs={4}>
+            Ne dorim sa facem planeta un loc mai verde pentru o viata mai
+            sanatoasa. Avem nevoie de oameni dedicati vin-o in echipa noastra.
+            Putem sesiza probleme, invata impreuna despre recilat si pe langa
+            acestea putem schimba obiecte intre noi.
+          </h4>
+          <Button variant="contained" href="/Inregistrare">
+            Sign Up
+          </Button>
+        </Grid>
+        <Grid item xs={4}>
           <h4 className="text-xl mb-1 font-semibold">
-          Pune un anunt sau colecteaza produsele oferite de alte persoane.
-                      </h4>
-                      <Button variant="contained" href="/Anunturi">Forum</Button>
-          </Grid>
-          </Grid>
-      
-        <Grid container spacing={2} justify="center" style={{ margin: "1.2em 0" }}>
-           <Grid item xs={2}>
-           <ImgMediaCard></ImgMediaCard></Grid>
-           <Grid item xs={2} >
-          <ImgMediaCard></ImgMediaCard></Grid>
-          <Grid item xs={2}>
-          <ImgMediaCard></ImgMediaCard></Grid>
-          <Grid item xs={2}>
-          <ImgMediaCard></ImgMediaCard></Grid>
+            Tu reciclezi, firmele te premiaza. Afla despre campaniile de
+            reciclare.{" "}
+          </h4>
+          <Button variant="contained" href="/CampaniiReciclare">
+            Campanii active
+          </Button>
         </Grid>
+      </Grid>
 
-        <Grid container spacing={2} justify="center" style={{ margin: "1.2em 0" }}>
-           <Grid item xs={2}>
-           <ImgMediaCard></ImgMediaCard></Grid>
-           <Grid item xs={2} >
-          <ImgMediaCard></ImgMediaCard></Grid>
-          <Grid item xs={2}>
-          <ImgMediaCard></ImgMediaCard></Grid>
-          <Grid item xs={2}>
-          <ImgMediaCard></ImgMediaCard></Grid>
+      <Grid
+        container
+        spacing={8}
+        justify="center"
+        style={{ margin: "1.2em 0" }}
+      >
+        <Grid item xs={4}>
+          <h4 className="text-xl mb-1 font-semibold">
+            Afla unde sunt cele mai apropiate puncte de colectare.
+          </h4>
+          <Button variant="contained" href="/PuncteReciclare">
+            Puncte de colectare
+          </Button>
         </Grid>
+        <Grid item xs={4}>
+          <h4 className="text-xl mb-1 font-semibold">
+            Pune un anunt sau colecteaza produsele oferite de alte persoane.
+          </h4>
+          <Button variant="contained" href="/Anunturi">
+            Forum
+          </Button>
+        </Grid>
+      </Grid>
 
+      <Grid
+        container
+        spacing={2}
+        justify="center"
+        style={{ margin: "1.2em 0" }}
+      >
+        <Grid item xs={2}>
+          <ImgMediaCard></ImgMediaCard>
+        </Grid>
+        <Grid item xs={2}>
+          <ImgMediaCard></ImgMediaCard>
+        </Grid>
+        <Grid item xs={2}>
+          <ImgMediaCard></ImgMediaCard>
+        </Grid>
+        <Grid item xs={2}>
+          <ImgMediaCard></ImgMediaCard>
+        </Grid>
+      </Grid>
+
+      <Grid
+        container
+        spacing={2}
+        justify="center"
+        style={{ margin: "1.2em 0" }}
+      >
+        <Grid item xs={2}>
+          <ImgMediaCard></ImgMediaCard>
+        </Grid>
+        <Grid item xs={2}>
+          <ImgMediaCard></ImgMediaCard>
+        </Grid>
+        <Grid item xs={2}>
+          <ImgMediaCard></ImgMediaCard>
+        </Grid>
+        <Grid item xs={2}>
+          <ImgMediaCard></ImgMediaCard>
+        </Grid>
+      </Grid>
     </Layout>
   );
 };
