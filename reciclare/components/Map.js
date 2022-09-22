7b43b38b2c185b14348561b7b8c7d26ad8547c4a
Nechifor I. Maria Bianca <bianca.nechifor@student.usv.ro>
2022-09-22 17:34:36 +0300

    The map made by Emilia

---
+++ reciclare/components/Map.js
@@ -0,0 +1,55 @@
+import React, { useRef, useEffect, useState } from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { initializeMap } from "../map/initializeMap";
+
+const mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
+
+function Map() {
+  const [pageIsMounted, setPageIsMounted] = useState(false);
+  const [Mapa, setMap] = useState();
+
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoibWFyaWFlbWlsaWE5ODIzNDc5MyIsImEiOiJjbDg4czdzYm8wMDVsM29vNXdqMnpyZWxkIn0.0MLW-8qKoYn3Y1uA32YDSQ";
+
+  useEffect(() => {
+    setPageIsMounted(true);
+
+    let map = new mapboxgl.Map({
+      container: "my-map",
+      style: "mapbox://styles/mariaemilia98234793/cl88sdkp7003h14qqb1paoqe1",
+      center: [27.587914, 47.158455],
+      zoom: 12,
+    });
+
+    initializeMap(mapboxgl, map);
+    setMap(map);
+  }, []);
+
+  /*  useEffect(() => {
+        if(pageIsMounted) {
+            Map.on("load", function () {
+                addDataLayer(Map, data);
+            });
+        }
+    }, [pageIsMounted, setMap, Map]);  */
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <main className={styles.main}>
+        <div id="my-map" style={{ height: 900, width: 900 }} />
+      </main>
+    </div>
+  );
+}
+
+export default Map;
--- reciclare/pages/PuncteReciclare.js
+++ reciclare/pages/PuncteReciclare.js
@@ -1,16 +1,15 @@
 import Layout from "components/layout/Layout";
 import { Container, Grid, Typography, Avatar, Box } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import Map from "components/Map.js";
 
-
-import * as React from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import ListItemText from '@mui/material/ListItemText';
-import BeachAccessIcon from '@mui/icons-material/BeachAccess';
-import Divider from '@mui/material/Divider';
-
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemText from "@mui/material/ListItemText";
+import BeachAccessIcon from "@mui/icons-material/BeachAccess";
+import Divider from "@mui/material/Divider";
 
 const useStyles = makeStyles((theme) => ({
   snsIcon: {
@@ -25,63 +24,78 @@ const useStyles = makeStyles((theme) => ({
       color: theme.palette.info.main,
     },
   },
- 
 }));
 
 const PuncteReciclare = () => {
   const classes = useStyles();
   // use your picture
-  
-  
+
   return (
-   
-          
     <Layout
       // type your page title and page description.
-      title=" Puncte de reciclare ">
-
-            <Typography variant="h1" align="center" gutterBottom>
-            Puncte de reciclare 
-            </Typography>      
-        
-        
-            
-
-          <Grid   align="right" >
-            
-          <List sx={{
-        width: '100%',
-        maxWidth: 360,
-        bgcolor: 'background.paper',
-      }}
+      title=" Puncte de reciclare "
     >
-      <Divider component="li" />
-      <ListItem>
-        <ListItemText primary="Firma1" secondary="Telefon: 07xx xxx xxx" />
-      </ListItem>
+      <Typography variant="h1" align="center" gutterBottom>
+        Puncte de reciclare
+      </Typography>
+      <Grid
+        container
+        spacing={6}
+        justify="center"
+        style={{ margin: "0em 1em" }}
+      >
+        {" "}
+        <Grid item>
+          <section>
+            <Map />
+          </section>
+        </Grid>
+        <Grid item>
+          <List
+            sx={{
+              width: "100%",
+              maxWidth: 360,
+              bgcolor: "background.paper",
+            }}
+          >
+            <ListItem>
+              <ListItemText primary="Firme de transport" />
+            </ListItem>
 
-      <Divider component="li" />
-      <ListItem>
-        <ListItemText primary="Firma2" secondary="Telefon: 07xx xxx xxx" />
-      </ListItem>
+            <Divider component="li" />
+            <ListItem>
+              <ListItemText
+                primary="Firma1"
+                secondary="Telefon: 07xx xxx xxx"
+              />
+            </ListItem>
 
-      <Divider component="li" />
-      <ListItem>
-        <ListItemText primary="Firma3" secondary="Telefon: 07xx xxx xxx" />
-      </ListItem>
+            <Divider component="li" />
+            <ListItem>
+              <ListItemText
+                primary="Firma2"
+                secondary="Telefon: 07xx xxx xxx"
+              />
+            </ListItem>
 
-      <Divider component="li" />
-      <ListItem>
-        <ListItemText primary="Firma4" secondary="Telefon: 07xx xxx xxx" />
-      </ListItem>
-    </List>
+            <Divider component="li" />
+            <ListItem>
+              <ListItemText
+                primary="Firma3"
+                secondary="Telefon: 07xx xxx xxx"
+              />
+            </ListItem>
 
-    
-            
-          </Grid>
-
-         
-      
+            <Divider component="li" />
+            <ListItem>
+              <ListItemText
+                primary="Firma4"
+                secondary="Telefon: 07xx xxx xxx"
+              />
+            </ListItem>
+          </List>
+        </Grid>
+      </Grid>
     </Layout>
   );
 };
